126c43be748dc84042ce3d607340ee46fb1093e6
slimane hassani <Slimane>
2018-08-01 22:27:50 +0200

    Maj Style changement police

--- js/main.js
+++ js/main.js
@@ -12,9 +12,15 @@ var messagesUn=document.getElementById("messagesUn")
 var messages2=document.getElementById("messages2")
 var messages3=document.getElementById("messages3")
 var messages4=document.getElementById("messages4")
+var btnLoock = document.getElementById("loock")
+var btnLoockDeux = document.getElementById("loockDeux")
+var imgUn =document.getElementById("imgUn")
+var imgDeux =document.getElementById("imgDeux")
+
 
 
 var messagesName =""
+var employeur =""
 
 function messagesBienvenue() {
 
@@ -23,6 +29,28 @@ function messagesBienvenue() {
 
     messagesUn.style.display="none"
     messages2.style.display="block"
+
+
+
+}
+
+
+function messagesBienvenueDeux() {
+
+
+    employeur = document.getElementById("formalertDeux").value ; 
+
+    messages2.style.display="none"
+    messages3.style.display="block"
+    btnLoock.style.display="block"
+    btnLoockDeux.style.display="none"
+    imgUn.style.display="none"
+    imgDeux.style.display="block"
+
+
+
+    document.getElementById("MerciName").innerHTML=messagesName ;
+
 
 
 
